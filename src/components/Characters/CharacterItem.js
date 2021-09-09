b8c479a541bfc15c69ae5b7b553b300fc063f547
juan17castillo <juancastillo9910@gmail.com>
2021-09-09 10:21:27 -0500

    useReducer and useCallback implementation

--- src/components/Characters/CharacterItem.js
+++ src/components/Characters/CharacterItem.js
@@ -1,29 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useReducer } from "react";
 import Card from "../UI/Card";
 import Modal from "../UI/Modal";
 import classes from "./CharacterItem.module.css";
+
+const modalReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_MODAL":
+      return { value: !state.value };
+    default:
+      return { value: false };
+  }
+};
+
 /**
  * Component in charge of rendering the information of a specific character with his name and photograph wrapped in a presentation card component.
  * @param {*} props with the information provided by CharacterList (photo, name, gender and status)
  * @returns {JSX.Element} in charge of rendering a character and detail information of each in a Modal.
  */
 const CharacterItem = (props) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
+  const [modalState, dispatchModal] = useReducer(modalReducer, {
+    value: false,
+  });
   /**
-   * Function in charge of being triggered when opening the information of a card to display the detailed information of a character. 
+   * Function in charge of being triggered when opening the information of a card to display the detailed information of a character.
    * This updates the state within the component.
    */
-  const handleOpenModal = () => {
-    setIsModalVisible(true);
-  };
-  /**
-   * Function in charge of being triggered when opening the information of a card to display the detailed information of a character. 
-   * This updates the state within the component.
-   */
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-  };
+  const handleToggleModal = useCallback(() => {
+    dispatchModal({ type: "TOGGLE_MODAL" });
+  }, [dispatchModal]);
 
   return (
     <>
@@ -31,14 +35,14 @@ const CharacterItem = (props) => {
         <Card
           picture={props.picture}
           name={props.name}
-          handleOpenModal={handleOpenModal}
+          handleOpenModal={handleToggleModal}
         />
       </li>
-      {isModalVisible && (
-        <Modal onClose={handleCloseModal}>
+      {modalState.value && (
+        <Modal onClose={handleToggleModal}>
           <>
             <div className={classes.closeModal}>
-              <button onClick={handleCloseModal}>X</button>
+              <button onClick={handleToggleModal}>X</button>
               <h1>{props.name}</h1>
             </div>
             <div className={classes.details}>
